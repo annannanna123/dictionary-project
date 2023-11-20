4e7ee5765904f72598d4b6940e763f4c87966d3f
annannanna123 <132966645+annannanna123@users.noreply.github.com>
2023-11-19 22:37:50 -0500

    Added dictionary search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Anna Chu</small>
+        </footer>
+      </div>
     </div>
   );
 }
